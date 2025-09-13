b2c4a4da3ce6bd662322dfcb4c693c76fe5b95c8
aftb777 <aftaabmulla228@gmail.com>
2025-09-13 14:58:36 +0530

    Basic Layout of the App

--- CurrencyConvertor/ContentView.swift
+++ CurrencyConvertor/ContentView.swift
@@ -8,8 +8,43 @@ import SwiftUI
 
 struct ContentView: View {
     
+    @State private var amount : String = ""
+    @State private var conversion : String = ""
+    @State private var currency : CurrencyChoice = .Usa
+    
     var body: some View {
-        
+        VStack (alignment: .leading) {
+            Text("Amount")
+                .font(.system(size: 18))
+                .bold(true)
+            
+            TextField("", text: $amount)
+                .font(.system(size: 18))
+                .padding(.top)
+                .overlay(RoundedRectangle(cornerRadius: 5)
+                    .stroke(Color.gray, lineWidth: 1))
+                
+            HStack{
+                // Spacer()
+                Image(systemName: "arrow.up.arrow.down")
+                    .font(.system(size: 20, weight: .bold))
+                    .frame(maxWidth: .infinity)
+                //Spacer()
+            }
+            .padding(.top, 25)
+            .padding(.bottom, 25)
+            
+            Text("Converted To")
+                .font(.system(size: 18))
+                .bold(true)
+            
+            TextField("", text: $conversion)
+                .font(.system(size: 18))
+                .padding(.top)
+                .overlay(RoundedRectangle(cornerRadius: 5)
+                    .stroke(Color.gray, lineWidth: 1))
+        }
+        .padding(.horizontal)
     }
 }
 
